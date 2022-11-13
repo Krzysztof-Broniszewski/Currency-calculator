2565c42931d4e5e0b2f3bf6a85b13b8829f0be0f
Krzysztof Broniszewski <k.broniszewski@gmail.com>
2022-11-13 11:43:09 +0100

    Initial commit

---
+++ JS/exchange.js
@@ -0,0 +1,58 @@
+{
+    let actualRateElement = document.querySelector(".js-form__value");
+    let resultElement = document.querySelector(".js-form__result");
+    let amountElement = document.querySelector(".js-form__amount");
+    let warningElement = document.querySelector(".js-form__warning");
+
+    const currencyElement = document.querySelector(".js-form__currency");
+    const EUR = 4.7146;
+    const USD = 4.7328;
+    const GBP = 5.3795;
+
+    currencyElement.addEventListener("input", () => {
+
+        let amount = amountElement.value
+        const currencyValue = () => {
+            switch (currencyElement.value) {
+                case "EUR":
+                    result = EUR * amount;
+                    actualRate = EUR;
+                    warningElement.innerText = "";
+                    break;
+                case "USD":
+                    result = USD * amount;
+                    actualRate = USD;
+                    warningElement.innerText = "";
+                    break;
+                case "GBP":
+                    result = GBP * amount;
+                    actualRate = GBP;
+                    warningElement.innerText = "";
+                    break;
+                case "Select currency":
+                    warningElement.innerText = "You must select a currency";
+            };
+        };
+
+        currencyValue();
+
+        actualRateElement.value = actualRate.toFixed(2);
+        resultElement.innerText = result.toFixed(2);
+    });
+
+    const displayDate = (Date) => {
+        const todayElement = document.querySelector(".js-form__date");
+        const data = new Date();
+        todayElement.innerText = data.toUTCString();
+    }
+
+    const init = () => {
+        displayDate(Date);
+    }
+
+    init()
+}
+
+
+
+
